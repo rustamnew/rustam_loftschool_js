44cc453ea42403e1c51bed2faf7d44bd9c1f0856
Rustam Yarullin <1rustamnew1@gmail.com>
2020-09-27 16:16:47 +0300

    1

--- projects/homework_3/index.js
+++ projects/homework_3/index.js
@@ -17,6 +17,21 @@
    isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false
  */
 function isAllTrue(array, fn) {
+  if (typeof fn != 'function') {
+    throw new Error('fn is not a function');
+  }
+
+  if (!Array.isArray(array) || !array.length) {
+    throw new Error('empty array');
+  };
+
+  for (let item of array) {
+    if (!fn(item)) {
+      return false
+    }
+  }
+
+  return true
 }
 
 /*
@@ -36,6 +51,21 @@ function isAllTrue(array, fn) {
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
  */
 function isSomeTrue(array, fn) {
+  if (typeof fn != 'function') {
+    throw new Error('fn is not a function');
+  }
+
+  if (!Array.isArray(array) || !array.length) {
+    throw new Error('empty array');
+  }
+
+  for (let item of array) {
+    if (fn(item)) {
+      return true
+    }
+  }
+
+  return false
 }
 
 /*
@@ -50,6 +80,23 @@ function isSomeTrue(array, fn) {
    - fn не является функцией (с текстом "fn is not a function")
  */
 function returnBadArguments(fn, ...args) {
+  if (typeof fn != 'function') {
+    throw new Error('fn is not a function');
+  }
+
+  let array = []
+
+  for (let item of args) {
+    try {
+      fn(item)
+    }
+
+    catch {
+      array.push(item)
+    }
+  }
+
+  return array
 }
 
 /*
@@ -70,6 +117,55 @@ function returnBadArguments(fn, ...args) {
    - какой-либо из аргументов div является нулем (с текстом "division by 0")
  */
 function calculator(number = 0) {
+  if (!Number.isFinite(number)) {
+    throw new Error('number is not a number');
+  }
+
+  return {
+    sum(...args) {
+      let result = number;
+
+      for (let item of args) {
+        result += item
+      }
+
+      return result
+    },
+
+    dif(...args) {
+      let result = number;
+
+      for (let item of args) {
+        result -= item
+      }
+
+      return result
+    },
+
+    div(...args) {
+      let result = number;
+
+      for (let item of args) {
+        if (item === 0) {
+          throw new Error('division by 0')
+        };
+
+        result /= item
+      }
+
+      return result
+    },
+
+    mul(...args) {
+      let result = number;
+
+      for (let item of args) {
+        result *= item
+      }
+
+      return result
+    }
+  }
 }
 
 /* При решении задач, постарайтесь использовать отладчик */
